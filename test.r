7873697232b18146199357d83c772ab3076963f1
mabobakr <m.abobakr10@yahoo.com>
2022-04-24 23:56:19 +0200

    Add ggplot plots

--- test.r
+++ test.r
@@ -1,4 +1,5 @@
 library("ggplot2")
+library(randomcoloR)
 setwd("D:\\Study\\Fourth year\\second term\\big data\\hotel-booking-demand-analysis")
 c <- "hello"
 data <- read.csv("hotel_bookings.csv")
@@ -61,3 +62,31 @@ barplot(hotel_repeted, legend.text = c("new","repeated"), col=colors)
 pie(table(data$previous_bookings_not_canceled)[-c(1, 2)])
 
 barplot(table(data$previous_bookings_not_canceled)[-1])
+
+month_cancel <- table(data$is_canceled, data$arrival_date_month)
+month_cancel
+
+g <- ggplot(data, aes(x = factor(arrival_date_month, levels = months) , fill = factor(is_canceled)))
+
+g + geom_bar(position = position_dodge()) + 
+labs(title = "Number of cancellations from each month\n", x = "Month", y = "Number of bookings")+
+scale_fill_discrete(name="", labels=c("Not Cancelled", "Cancelled"))
+
+
+country_city <- table(data[data$hotel == "City Hotel",]$country)
+
+country_city
+
+pie(country_city)
+
+color = grDevices::colors()[grep('gr(a|e)y', grDevices::colors(), invert = T)]
+cols <- sample(color, 178)
+
+country_city <- as.data.frame(country_city)
+country_city
+ggplot(country_city, aes(x = "", y = sort(Freq), colour=Var1, fill= Var1))+
+ geom_col(color = "transparent", width = NULL) +
+  scale_fill_manual(values = sample(color, 178))+
+  coord_polar("y", start = 0) +
+  theme_void()
+
