d8949e241ea9072d5888ff81a6575c82951f7581
IslamAhmed1092 <eslam1092@hotmail.com>
2022-05-06 14:21:36 +0200

    add mapreduce for logistic regression and test it

---
+++ LogReg/mapper.py
@@ -0,0 +1,24 @@
+#!/home/islamahmed1092/anaconda3/bin/python
+
+import sys
+import numpy as np
+
+data = np.array([np.array([i for i in line.strip().split(',')], dtype=np.float64) for line in sys.stdin])
+X_train = np.pad(data[:, :-1], [(0,0), (1,0)], mode='constant', constant_values=1)
+y_train = data[:, -1:]
+
+epochs = int(sys.argv[1])
+alpha = float(sys.argv[2])
+w = np.zeros((X_train.shape[1], 1))
+
+for epoch in range(epochs):
+    for i in range(X_train.shape[0]):
+        x = X_train[i].reshape(-1,1)
+        y = y_train[i]
+        
+        yhat = (1/(1+np.exp(-(w.T @ x)))).squeeze()
+        grad = (yhat - y) * x
+        w = w - alpha * grad
+
+for i in range(w.shape[0]):
+    print(f'{i}\t{w[i][0]}')
---
+++ LogReg/reducer.py
@@ -0,0 +1,22 @@
+#!/home/islamahmed1092/anaconda3/bin/python
+
+import sys
+import numpy as np
+
+
+prev_key = '0'
+summation = 0
+length = 0
+
+for line in sys.stdin:
+    key, val = line.strip().split('\t', 1)
+    if key != prev_key:
+        print(f'{prev_key},{summation/length}')
+        summation = 0
+        length = 0
+
+    summation += float(val)
+    length += 1   
+    prev_key = key
+
+print(f'{prev_key},{summation}')
